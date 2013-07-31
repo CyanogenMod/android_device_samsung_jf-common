a88335837e0a508cbd744adfa97288bbc8cc5640
David Ferguson <ferguson.david@gmail.com>
2013-07-31 10:57:15 -0400

    jf: Support Samsung extended AGPS
      The GPS chipset used in the phone can use the Psc value for AGPS.
      Add the new define and the missing field in AGpsRefLocationCellID.
    
      References:
        http://review.cyanogenmod.org/#/c/45564/
        http://review.cyanogenmod.org/#/c/45565/
    
    Change-Id: I815dc4155cb624f0a90ed7cff98a132baca2d13b

--- include/hardware/gps.h
+++ include/hardware/gps.h
@@ -407,6 +407,11 @@ typedef uint16_t NavigationMessageStatus;
 #define AGPS_RIL_INTERFACE      "agps_ril"
 
 /**
+ * The GPS chipset can use Psc for AGPS
+ */
+#define AGPS_USE_PSC
+
+/**
  * Name for the GPS_Geofencing interface.
  */
 #define GPS_GEOFENCING_INTERFACE   "gps_geofencing"
@@ -509,6 +514,9 @@ typedef struct {
     uint16_t mcc;
     uint16_t mnc;
     uint16_t lac;
+#ifdef AGPS_USE_PSC
+    uint16_t psc;
+#endif
     uint32_t cid;
 } AGpsRefLocationCellID;
 
