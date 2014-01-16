aef67a5e06099ae610607ffe50912306be95e71c
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-15 23:46:51 -0600

    jflte: make tree universal.
    * do what hammerhead did.
    * shove cdma and gsm into one universal tree
    
    Change-Id: I4f2063ad5296297a945976ca011823c2a0487129

---
+++ cm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/samsung/jflte/full_jflte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jflte \
+    TARGET_DEVICE=jflte \
+    BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" \
+    PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+
+PRODUCT_DEVICE := jflte
+PRODUCT_NAME := cm_jflte
