c5804047176822dd1f835591a35807fd37b01443
Drew Davis <dcd1182@gmail.com>
2014-11-15 17:48:42 -0600

    jf: inherit qcom sepolicy
    Change-Id: Iee2b5819c5e23182abc96e1b0268d682ca74edf0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,33 +124,6 @@ TARGET_RECOVERY_FSTAB := device/samsung/jf-common/rootdir/etc/fstab.qcom
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/jf-common/releasetools
 
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/jf-common/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file_contexts \
-    app.te \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file.te \
-    hci_init.te \
-    healthd.te \
-    init.te \
-    init_shell.te \
-    keystore.te \
-    kickstart.te \
-    mediaserver.te \
-    nfc.te \
-    rild.te \
-    surfaceflinger.te \
-    system.te \
-    ueventd.te \
-    wpa.te \
-    wpa_socket.te
-
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
