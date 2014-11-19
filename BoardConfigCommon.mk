fc3bed2dcae0ae82598b9c808eb1ccc1cf3942f7
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-19 13:55:52 -0600

    jf: use only device-specific sepolicies
    Change-Id: Ie4e7beac82ff5e7ed0f562cf4cfb9d61cc0c0506

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,27 +140,9 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung/jf-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    file_contexts \
-    app.te \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file.te \
-    hci_init.te \
     healthd.te \
     init.te \
-    init_shell.te \
-    keystore.te \
-    kickstart.te \
-    mediaserver.te \
-    nfc.te \
-    rild.te \
-    surfaceflinger.te \
-    system.te \
-    ueventd.te \
-    wpa.te \
-    wpa_socket.te
+    mm-pp-daemon.te
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
