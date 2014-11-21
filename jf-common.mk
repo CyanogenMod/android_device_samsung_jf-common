ac5fcd55c05eece0fcf12603440117c0d8b7ffa1
dcd <dcd1182@gmail.com>
2014-11-20 18:28:30 -0600

    jf-common: update thermal-engine confs
    Change-Id: If3296198b9c8ad7c30c2e9b68e8f8b63a0270e44

--- jf-common.mk
+++ jf-common.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf
 
 # Torch
 PRODUCT_PACKAGES += \
