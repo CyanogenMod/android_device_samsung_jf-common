8ca873a8b2207851c12d180875b08c5e442d89d1
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-24 17:24:22 -0600

    sepolicy: add device-specific sepolicies
    Change-Id: Ibfce7ff8fcd2ca42d8e6b9b20eb662538a0915ce

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,6 +136,25 @@ TARGET_RECOVERY_FSTAB := device/samsung/jf-common/rootdir/etc/fstab.qcom
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/jf-common/ril
 
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += device/samsung/jf-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    healthd.te \
+    insthk.te \
+    mdm_helper.te \
+    mm-pp-daemon.te \
+    mm-qcamerad.te \
+    mpdecision.te \
+    rild.te \
+    system_server.te \
+    thermal-engine.te \
+    ueventd.te \
+    vold.te \
+    wpa.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
