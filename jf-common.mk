7164606c137e7e19074b0a242ff72e8fe3d8f5f9
dcd <dcd1182@gmail.com>
2014-11-27 16:55:50 -0600

    jf: more wifi updates for L
    Change-Id: I8f230db9084b1f12a62209570477feb4fe117cfd

--- jf-common.mk
+++ jf-common.mk
@@ -202,10 +202,12 @@ PRODUCT_PACKAGES += \
     macloader \
     dhcpcd.conf \
     hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf
+    hostapd_default.conf \
+    libwpa_client \
+    wpa_supplicant
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
