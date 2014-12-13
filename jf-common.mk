0fbb67a4d10a28953baff4a8deb00b2b1c11a5e8
dcd <dcd1182@gmail.com>
2014-12-13 02:22:24 -0600

    jf: merge I9505GUEUDNL3 init, configs
    Change-Id: I54273451c5ecef4948b57911dfa932dd7feb09b5

--- jf-common.mk
+++ jf-common.mk
@@ -188,7 +188,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf
 
