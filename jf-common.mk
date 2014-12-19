06e070daacade91101caabbe5a28fda281f63010
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-19 10:27:11 -0600

    Revert "Revert "jf: Add shell property to use AwesomePlayer as default.""
    This reverts commit baab5d0dd09fb4e9f37a7b6e2ab274cf0cf6cb24.
    
    Change-Id: Ieeaf095d40ae9bfd29c923304177cc66289708c6

--- jf-common.mk
+++ jf-common.mk
@@ -227,6 +227,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608 \
     ro.sf.lcd_density=480
 
+# media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.use-awesome=true
+
 # radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.ds_fmc_app.mode=0 \
