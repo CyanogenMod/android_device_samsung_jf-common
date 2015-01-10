9294820a593b2de8fb073e596c4e51db7a10fd93
AntaresOne <antaresone@antaresone.eu>
2015-01-10 14:51:21 +0100

    jf: add F2FS support
    Change-Id: I3bd4565033009b731f48241a3764518945d27e6f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,30 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := busybox
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/sbin/ext/busybox
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin/ext
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := blkid
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/sbin/ext/blkid
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin/ext
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fscheck.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fscheck.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
