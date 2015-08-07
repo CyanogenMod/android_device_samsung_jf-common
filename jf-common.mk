e81d57253c1bac7bd17db737e6d77ae99faca0a9
dcd <dcd1182@gmail.com>
2015-08-07 16:25:05 -0500

    jf: add ro.telephony.mms_data_profile=5
    Change-Id: Ia9ffed3fc6c426e8e5be596d43833272e505959b

--- jf-common.mk
+++ jf-common.mk
@@ -238,7 +238,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.fill_eons=1 \
     persist.radio.use_se_table_only=1 \
     ro.telephony.ril.config=newDriverCallU,newDialCode \
-    ro.ril.telephony.mqanelements=6
+    ro.ril.telephony.mqanelements=6 \
+    ro.telephony.mms_data_profile=5
 
 # gps
 PRODUCT_PROPERTY_OVERRIDES += \
