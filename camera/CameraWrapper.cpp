a8e4797748e3b1231e2ce496e857241d65133e47
dcd <dcd1182@gmail.com>
2015-09-08 11:33:50 -0700

    Revert "jf: camera: dont advertise HDR"
    we should only hit the rear cam with scene-modes
    
    This reverts commit eb5a0632c69c1152a5d8fcbed14ce5df84ec71e0.
    
    Change-Id: Ib31fc4b19e08fa3e64e9e52f69bad553af70d0fc

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -110,9 +110,6 @@ static char * camera_fixup_getparams(int id, const char * settings)
     // fix params here
     params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
 
-    /* Remove HDR */
-    params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES, "auto,action,night,sunset,party");
-
     /* Enforce video-snapshot-supported to true */
     params.set(android::CameraParameters::KEY_VIDEO_SNAPSHOT_SUPPORTED, "true");
 
