791308e7bf9cf0b2f44c3289f5277601358b7ac5
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-05 08:38:41 -0600

    jf: update blob list and extraction scripts
    Change-Id: Ie95d28cdaea9e69e0db9415a4b2ab69676e4e521

--- extract-files.sh
+++ extract-files.sh
@@ -46,13 +46,19 @@ else
   fi
 fi
 
-BASE=../../../vendor/$VENDOR/jf-common/proprietary
-rm -rf $BASE/*
+BLOBLOC=$DEVICE
+if [ "$DEVICE" == "jflteatt" ] || [ "$DEVICE" == "jfltecan" ] || [ "$DEVICE" == "jfltetmo" ] || [ "$DEVICE" == "jfltexx" ]; then
+    BLOBLOC=jf-gsm-common
+fi
 
-DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVBASE/*
 
-extract ../../$VENDOR/jf-common/common-proprietary-files.txt $BASE
-extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVBASE
+BASE=../../../vendor/$VENDOR/jf-common
+rm -rf $BASE
+
+DEVBASE=../../../vendor/$VENDOR/$BLOBLOC
+rm -rf $DEVBASE
+
+extract ../../$VENDOR/jf-common/common-proprietary-files.txt $BASE/proprietary
+extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVBASE/proprietary
 
 ../jf-common/setup-makefiles.sh
