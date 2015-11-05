c902b089c20572c8491c73e21457269179b36be3
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-05 07:56:29 -0600

    jf: move vidc firmware links to variant trees
    * Some variants ship these blobs
    
    Change-Id: I3509f3566ff1ed6de6bf4d148b47a52c85d03bda

--- Android.mk
+++ Android.mk
@@ -52,8 +52,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MDM_SYMLINKS)
 # Create /firmware links
 FIRMWARE_IMAGES := \
     q6.b00 q6.b01 q6.b03 q6.b04 q6.b05 q6.b06 q6.mdt \
-    tzapps.b00 tzapps.b01 tzapps.b02 tzapps.b03 tzapps.mdt \
-    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+    tzapps.b00 tzapps.b01 tzapps.b02 tzapps.b03 tzapps.mdt
 
 FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
 $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
