de72e5de646e0f4b7d1f9a28434a9aa645db680d
Arne Coucheron <arco68@gmail.com>
2015-11-23 08:01:50 -0600

    jf: Explicitly build all media related packages
    * Recent change in media HAL source doesn't pull these in
      automatically anymore.
    
    Change-Id: Iff776aa53a8ed67012a5b44492c9f3ce4cec3a28

--- jf-common.mk
+++ jf-common.mk
@@ -162,7 +162,12 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
     libdashplayer \
+    libdashplayer \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
     libOmxVdec \
     libOmxVenc \
     libOmxAacEnc \
