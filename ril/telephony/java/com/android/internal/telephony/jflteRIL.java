66c854f5c2911c07e16b3293ad2868c83196d277
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-25 23:54:33 -0600

    jflteRIL: remove setting mQANElements
    * this is handled via the prop in the super class now
    
    Change-Id: I88a1efd99800d1a097fdb8b1f9baa2b500687a8e

--- ril/telephony/java/com/android/internal/telephony/jflteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/jflteRIL.java
@@ -70,14 +70,12 @@ public class jflteRIL extends RIL implements CommandsInterface {
     public jflteRIL(Context context, int networkModes, int cdmaSubscription) {
         this(context, networkModes, cdmaSubscription, null);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-        mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
 
     public jflteRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-        mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
 
     @Override
