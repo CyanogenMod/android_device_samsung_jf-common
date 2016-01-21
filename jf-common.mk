4e9cb8cb7bb58eb983cb0f723dd2184a26f6c383
B--B <dark.schneider.ds@gmail.com>
2016-01-21 01:54:59 +0100

    jf: ril: Fix network search
    Big thanks to Alucard24 for his great help! :)
    
    Change-Id: I16a395e8d6113eadc56586b3a19948aef8fe669a

--- jf-common.mk
+++ jf-common.mk
@@ -237,7 +237,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.fill_eons=1 \
     persist.radio.use_se_table_only=1 \
-    ro.ril.telephony.mqanelements=6 \
     ro.telephony.mms_data_profile=5
 
 # gps
--- ril/telephony/java/com/android/internal/telephony/jflteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/jflteRIL.java
@@ -576,38 +576,6 @@ public class jflteRIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
-    //this method is used in the search network functionality.
-    // in mobile network setting-> network operators
-    @Override
-    protected Object
-    responseOperatorInfos(Parcel p) {
-        String strings[] = (String [])responseStrings(p);
-        ArrayList<OperatorInfo> ret;
-
-        if (strings.length % mQANElements != 0) {
-            throw new RuntimeException(
-                                       "RIL_REQUEST_QUERY_AVAILABLE_NETWORKS: invalid response. Got "
-                                       + strings.length + " strings, expected multiple of " + mQANElements);
-        }
-
-        ret = new ArrayList<OperatorInfo>(strings.length / mQANElements);
-        Operators init = null;
-        if (strings.length != 0) {
-            init = new Operators();
-        }
-        for (int i = 0 ; i < strings.length ; i += mQANElements) {
-            String temp = init.unOptimizedOperatorReplace(strings[i+0]);
-            ret.add (
-                     new OperatorInfo(
-                                      temp, //operatorAlphaLong
-                                      temp,//operatorAlphaShort
-                                      strings[i+2],//operatorNumeric
-                                      strings[i+3]));//state
-        }
-
-        return ret;
-    }
-
     @Override
     public void getImsRegistrationState(Message result) {
         if(mRilVersion >= 8)
