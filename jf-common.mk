2d49ce4af778200c75fe82851ae22cbcb5a1aa48
Ethan Chen <intervigil@gmail.com>
2016-02-09 01:22:54 -0800

    jf: Remove old CRDA packages
    * These have no target anymore.
    
    Change-Id: I401c9a1c1303e7b7d38bace6a9d3a8a5d85668f7

--- jf-common.mk
+++ jf-common.mk
@@ -92,13 +92,6 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.msm8960
 
-# CRDA
-PRODUCT_PACKAGES += \
-    crda \
-    linville.key.pub.pem \
-    regdbdump \
-    regulatory.bin
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8960 \
