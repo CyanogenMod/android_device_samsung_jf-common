075f9c480cb3241657dcc44a3eff3a0181eda819
Grando F <aqdevmain@gmail.com>
2016-05-12 10:04:47 -0700

    jf: Shift to new audio HAL
     * Add audio effects
     * Add custom audio platform info
     * Add custom mixer paths
     * Update audio policy
     * Update audio properties, flags and packages
     * Remove deprecated audio configs
    
    Many thanks to arco, whose serrano work this is based on.
    
    Change-Id: I8e7d1056b9c26c3f03380a2319b7b9d659c2b5d3

--- jf-common.mk
+++ jf-common.mk
@@ -75,12 +75,15 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8960 \
     audio.r_submix.default \
     audio.usb.default \
-    libaudio-resampler
+    libaudio-resampler \
+    libqcomvoiceprocessing
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
@@ -211,11 +214,8 @@ PRODUCT_COPY_FILES += \
 
 # audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.vr.enable=false \
-    persist.audio.handset.mic=digital \
-    persist.audio.speaker.location=high \
-    ro.qc.sdk.audio.fluencetype=fluence
+    audio.offload.disable=1 \
+    use.dedicated.device.for.voip=true
 
 # charging mode
 PRODUCT_PROPERTY_OVERRIDES += \
