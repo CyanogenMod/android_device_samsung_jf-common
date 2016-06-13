f3c40ae98d63007ed9e77f0ccc02d304e84b247e
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 13:27:00 -0500

    cmhw: Use cmsdk FileUtils
    Change-Id: Idc3cfef4fb80e9264ca4863ffbb04f9f54b477c6

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -18,7 +18,7 @@ package org.cyanogenmod.hardware;
 
 import java.io.File;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -38,8 +38,7 @@ public class KeyDisabler {
      */
 
     public static boolean isSupported() {
-        File f = new File(KEYDISABLER_PATH);
-        return f.exists();
+        return FileUtils.isFileWritable(KEYDISABLER_PATH);
     }
 
     /*
