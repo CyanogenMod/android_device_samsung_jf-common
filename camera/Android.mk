ffbbf301c373296543f4407f10731d4b5bfecfaf
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-07-10 12:18:08 +0800

    jf: Update module path reference
    Since L, the appropriate reference for shared libraries
    should be LOCAL_MODULE_RELATIVE_PATH.
    
    For 32-bit devices, this is merely a cosmetic change.
    
    Change-Id: I2176df71a4ea1162e5f979317cb0b49e7284bc6f

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,7 +10,7 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.msm8960
 LOCAL_MODULE_TAGS := optional
 
