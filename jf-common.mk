fa811c75cbdc5a600c18d8bab3c12f5b6b6db443
Arne Coucheron <arco68@gmail.com>
2016-07-12 06:35:54 +0200

    jf: Shift to new audio HAL - Take 2
     * Add audio effects
     * Add custom audio platform info
     * Add custom mixer paths
     * Update audio policy
     * Update audio properties, flags and packages
     * Remove deprecated audio configs
    
    Change-Id: I95f95ca1c79e53b98a53089822fdac35ccc92aa9

--- jf-common.mk
+++ jf-common.mk
@@ -72,12 +72,15 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8960 \
     audio.r_submix.default \
     audio.usb.default \
-    libaudio-resampler
+    libaudio-resampler \
+    libqcomvoiceprocessing
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
