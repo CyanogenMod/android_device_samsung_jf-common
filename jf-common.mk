73b14d12e930c44987f84afb2842271b7ad30704
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-07-20 00:01:46 +0800

    jf: Re-add thermal engine
    The in-kernel thermal driver is way too simplified,
    which leads to bad thermal control decisions.
    
    Switch back to the tried and tested thermal-engine daemon.
    
    This mostly reverts commit 5e38d6ec22e4ada3ee02390882a3e230399ac8fd.
    
    Change-Id: Ife519f3c30f1c603ca2bf11e5cf9ef8562843566

--- jf-common.mk
+++ jf-common.mk
@@ -195,6 +195,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
