8541a969689fb5c304211b56ecb0becc3026eb17
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-07-22 23:04:00 +0800

    jf: Clean out init
     - Reorganise init scripts
     - Clean out unnecessary ops, services and props
     - Start kickstart and insthk on class core
     - Move ops to their appropriate locations
     - Move /dev perms to ueventd
     - Merge QC and target init scripts
    
    Change-Id: I92b8b0d30ef8df97af9b4db2b627cc73d8275f4a

--- jf-common.mk
+++ jf-common.mk
@@ -178,11 +178,9 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.carrier.rc \
     init.qcom.rc \
     init.qcom.power.rc \
     init.qcom.usb.rc \
-    init.target.rc \
     ueventd.qcom.rc
 
 # Samsung symbols
