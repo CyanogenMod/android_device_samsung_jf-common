90f6729eeebd7beae2f39bb344d49695680d30b9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-04 20:39:38 +0800

    Revert "Revert "jf: ril: remove lollipop hack""
    The issue previously mentioned doesn't seem to happen anymore.
    
    This reverts commit 3e74ab7c8203a5725b533ac4b03901291f0530d5.
    
    Change-Id: Iae37d9915633653b48a3b5473d31656442b53d90

--- ril/telephony/java/com/android/internal/telephony/jflteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/jflteRIL.java
@@ -66,8 +66,6 @@ public class jflteRIL extends RIL implements CommandsInterface {
     protected boolean isGSM = false;
     public static final long SEND_SMS_TIMEOUT_IN_MS = 30000;
 
-    private Message mPendingGetSimStatus;
-
     public jflteRIL(Context context, int networkModes, int cdmaSubscription) {
         this(context, networkModes, cdmaSubscription, null);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
@@ -587,30 +585,6 @@ public class jflteRIL extends RIL implements CommandsInterface {
                 AsyncResult.forMessage(result, null, ex);
                 result.sendToTarget();
             }
-        }
-    }
-
-    // Hack for Lollipop
-    // The system now queries for SIM status before radio on, resulting
-    // in getting an APPSTATE_DETECTED state. The RIL does not send an
-    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
-    // initialized, so delay the message until the radio is on.
-    @Override
-    public void getIccCardStatus(Message result) {
-        if (mState != RadioState.RADIO_ON) {
-            mPendingGetSimStatus = result;
-        } else {
-            super.getIccCardStatus(result);
-        }
-    }
-
-    @Override
-    protected void switchToRadioState(RadioState newState) {
-        super.switchToRadioState(newState);
-
-        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            super.getIccCardStatus(mPendingGetSimStatus);
-            mPendingGetSimStatus = null;
         }
     }
 
