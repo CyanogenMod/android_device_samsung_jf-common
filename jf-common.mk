bdbd7fd0b3390b331e3b283512ee6d189c0c0b71
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-11 12:52:07 +0800

    jf: gps: Remove unused izat config
    This config merely sets what is already the default.
    Furthermore, Stock GPE and TW don't use nor have this config.
    
    Change-Id: I5a3991e85bf6b2882798debb77f0a10eb5934479

--- jf-common.mk
+++ jf-common.mk
@@ -106,8 +106,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf \
-    $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf
+    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
 
 # Gello
 PRODUCT_PACKAGES += \
