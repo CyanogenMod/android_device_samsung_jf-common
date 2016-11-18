a50c720d2deb78a1c7fa5e50384fb73fab0ed47b
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-17 21:20:05 -0600

    jf: specify no misc partition
    Change-Id: I9c9c9e09be6e56bc3c6b7a098a8b70b056db02fb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.f
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
+TARGET_HAS_NO_MISC_PARTITION := true
 TARGET_KERNEL_CONFIG := cyanogenmod_jf_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
 
