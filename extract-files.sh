bca3fb4604d98e2e42be3fce7595fcc730f76087
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-01 12:03:43 +0800

    jf: Actually extract the graphics libraries
    Change-Id: I6652ab2bfee4d3703b23230bf81ddab90a208c69

--- extract-files.sh
+++ extract-files.sh
@@ -69,3 +69,5 @@ setup_vendor "$BLOB_LOC" "$VENDOR" "$CM_ROOT"
 extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh
+
+"$CM_ROOT"/device/qcom/common/extractors/extract-files.sh "$SRC" msm8960 graphics
